d474c11f3a6a4c40e69e7f4048e0b6f0941cc97e
thePeeyush <peeyushdehariya951@gmail.com>
2023-08-19 02:02:31 +0530

    login method update

--- src/utility/authsession.js
+++ src/utility/authsession.js
@@ -2,35 +2,34 @@ import { getData } from "@/components/payment";
 import { default as account } from "./account.config";
 import { passData } from "./authverify";
 
-export const createAccount = (data , setSubmit,setIsVerified) => {
-
+export const createAccount = (data, setSubmit, setIsVerified) => {
   const promise = account.get();
 
-  promise.then(function (response) {
-      if (response.status === true) {
-        getData(data);
-        setSubmit(true);
-        setIsVerified(true);
-      } else {
-        const phone = data.phone;
-        const userID = "tasty" + phone;
-      
-        const promise = account.createPhoneSession(userID, `+91${phone}`);
-      
-        promise.then(
-          function (response) {
-            console.log(response); // Success
-            passData(data);
-            setSubmit(true);
-          },
-          function (error) {
-            console.log(error); // Failure
-          }
-        );
-      }
-  }, function (error) {
+  promise.then(
+    function (response) {
+      console.log(response); // Success
+      passData(data);
+      getData(data);
+      setSubmit(true);
+      setIsVerified(true);
+    },
+    function (error) {
       console.log(error); // Failure
-      alert(error)
-  });
+      const phone = data.phone;
+      const userID = "tasty" + phone;
 
+      const promise = account.createPhoneSession(userID, `+91${phone}`);
+
+      promise.then(
+        function (response) {
+          console.log(response); // Success
+          passData(data);
+          setSubmit(true);
+        },
+        function (error) {
+          console.log(error); // Failure
+        }
+      );
+    }
+  );
 };

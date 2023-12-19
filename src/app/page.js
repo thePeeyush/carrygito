809509b41731b52e5b186a702342dc9cb0464797
thePeeyush <peeyushdehariya951@gmail.com>
2023-12-20 00:48:47 +0530

    use client changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,3 @@
-"use client";
-
 import Footer from '@/components/footer';
 import Sections from '@/components/sections';
 
--- src/components/plans.jsx
+++ src/components/plans.jsx
@@ -1,5 +1,4 @@
 
-
 export default function Plans({ourPlans}) {
 
   return (
